d45c78ba7fc2069d2cebe44b6af2034a21b9cd7d
Chris Hurst <chris.hurst@stratusgrid.com>
2019-08-21 12:02:54 -0400

    attached read only access to role

--- iam-read-only-policy.tf
+++ iam-read-only-policy.tf
@@ -1,8 +1,14 @@
+data "aws_iam_policy" "read_only_access" {
+  arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
 resource "aws_iam_group_policy_attachment" "read_only_access" {
   group      = aws_iam_group.group.name
   policy_arn = data.aws_iam_policy.read_only_access.arn
 }
 
-data "aws_iam_policy" "read_only_access" {
-  arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+resource "aws_iam_role_policy_attachment" "read_only_access" {
+  role       = aws_iam_role.this.name
+  policy_arn = data.aws_iam_policy.read_only_access.arn
 }
+
--- iam-role.tf
+++ iam-role.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "this" {
   assume_role_policy = data.aws_iam_policy_document.cross_account_assume_role_policy_mfa.json
 }
 
-resource "aws_iam_role_policy_attachment" "cross_account_assume_role" {
+resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
   policy_arn = aws_iam_policy.this.arn
 }
