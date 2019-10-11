16c2f3ce6c8a797aa6480f4f0ee77af392d2b274
Chris Hurst <chris.hurst@stratusgrid.com>
2019-10-11 11:37:57 -0400

    updated outputs and readme

--- -outputs.tf
+++ -outputs.tf
@@ -4,7 +4,11 @@ output "iam_group_id" {
 }
 
 output "iam_role_arn" {
-  description = "ARN of IAM Group created"
-  value       = aws_iam_group.group.arn
+  description = "ARN of IAM role created"
+  value       = aws_iam_role.this.arn
 }
 
+output "iam_role_assumption_url" {
+  description = "URL to assume role in Console"
+  value       = "https://signin.aws.amazon.com/switchrole?account=${data.aws_caller_identity.current.account_id}&roleName=${aws_iam_role.this.name}&displayName=${aws_iam_role.this.name}"
+}
