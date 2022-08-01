c646e34c8a4d849412e50bc20188ec2a1c1c9551
Wesley Kirkland <92735382+wesleykirklandsg@users.noreply.github.com>
2022-08-01 14:19:43 -0400

    IAM EOF to Data Block (#2)
    * First 2 policy changes
    
    * Rename and move policy data block
    
    * Fixed spacing
    
    * Correct to not_actions
    
    * Removed variable interpolation
    
    Co-authored-by: Wesley Kirkland <wesley.kirkland@stratusgrid.com>

---
+++ iamdata.tf
@@ -0,0 +1,162 @@
+data "aws_iam_policy_document" "user_self_service_policy" {
+  statement {
+    sid = "ListUsersAccessWithoutMFA"
+
+    actions = [
+      "iam:ListVirtualMFADevices"
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    sid = "SelfServiceAccessWithoutMFA"
+
+    actions = [
+        "iam:List*",
+        "iam:GetUser",
+        "iam:GetAccountPasswordPolicy",
+        "iam:ChangePassword",
+        "iam:CreateVirtualMFADevice",
+        "iam:EnableMFADevice"
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:iam::*:user/$${aws:username}",
+      "arn:aws:iam::*:mfa/$${aws:username}"
+    ]
+  }
+
+  statement {
+    sid = "SelfServiceAccessWithMFA"
+
+    actions = [
+        "iam:Get*",
+        "iam:DeleteSSHPublicKey",
+        "iam:GetSSHPublicKey",
+        "iam:ListSSHPublicKeys",
+        "iam:UpdateSSHPublicKey",
+        "iam:UploadSSHPublicKey",
+        "iam:CreateAccessKey",
+        "iam:DeleteAccessKey",
+        "iam:UpdateAccessKey",
+        "iam:DeleteVirtualMFADevice",
+        "iam:DeactivateMFADevice",
+        "iam:ResyncMFADevice",
+        "iam:UploadSigningCertificate",
+        "iam:UpdateSigningCertificate",
+        "iam:DeleteSigningCertificate",
+        "iam:GenerateServiceLastAccessedDetails"
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:iam::*:user/$${aws:username}",
+      "arn:aws:iam::*:mfa/$${aws:username}"
+    ]
+
+    condition {
+      test     = "BoolIfExists"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "true"
+      ]
+    }
+  }
+
+  statement {
+    sid = "DenyAllExceptListedIfNoMFA"
+
+    not_actions = [
+        "iam:ListVirtualMFADevices",
+        "iam:List*",
+        "iam:GetUser",
+        "iam:GetAccountPasswordPolicy",
+        "iam:EnableMFADevice",
+        "iam:CreateVirtualMFADevice",
+        "iam:ChangePassword"
+    ]
+
+    effect = "Deny"
+
+    resources = [
+        "*"
+    ]
+
+    condition {
+      test     = "BoolIfExists"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "false"
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "role_assumption" {
+  statement {
+    sid = "AssumeRole"
+
+    actions = [
+        "sts:AssumeRole"
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      aws_iam_role.this.arn
+    ]
+
+    condition {
+      test     = "BoolIfExists"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        "true"
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "deny_data_retrieval" {
+  statement {
+    sid = "DenyReadOnlyDataRetrieval"
+    effect = "Deny"
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetObjectVersionForReplication",
+      "lambda:GetFunction",
+      "workdocs:Get*",
+      "workmail:Get*",
+      "athena:GetQueryResults*",
+    ]
+    not_resources = var.s3_bucket_paths_to_allow
+  }
+  statement {
+    sid = "DenyReadOnlySecrets"
+    effect = "Deny"
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "ssm:GetParameter*",
+      "ecr:GetAuthorizationToken",
+      "ec2:GetPasswordData",
+      "redshift:GetClusterCredentials",
+      "cognito-identity:GetCredentialsForIdentity",
+      "cognito-identity:GetId",
+      "cognito-identity:GetOpenIdToken",
+    ]
+    resources = [
+      "*",
+    ]
+  }
+}
